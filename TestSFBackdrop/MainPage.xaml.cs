a7823769b5b21b22b6143443b2387e53fa94aa36
Rob Kerr <mail@robkerr.com>
2019-08-11 22:01:36 -0400

    First commit

---
+++ TestSFBackdrop/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Syncfusion.XForms.Backdrop;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestSFBackdrop
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : SfBackdropPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
